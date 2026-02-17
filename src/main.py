570904c693f1047f99753136cf1c13dfb615e9a6
Inish Codes <inishbista20@gmail.com>
2026-02-16 22:32:54 -0500

    "Feat: Add Websocket live transcripion endpoint to transcribe the audio from OpenAI Whisper API" - Convert the raw audio bytes into Wav format to match Whisper API audio types - Apply asyncio to process transcription concurrently so that audio_collection through websocket isnt dropped due to audio_buffer overflow

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,  File, UploadFile, HTTPException, Depends, Request
+from fastapi import FastAPI,  File, UploadFile, HTTPException, Depends, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -6,6 +6,9 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import io
+import asyncio
+import array
+import wave
 from typing import Tuple
 from validators import validate_audio_file
 from pathlib import Path
@@ -90,7 +93,89 @@ async def transcribe_audio(
         raise HTTPException(status_code=500, detail=f"Transcription Failed: {str(e)}")
 
 
-    #Save transcripts to the file
+
+@app.websocket("/ws/transcribe")
+async def websocket_transcribe(websocket: WebSocket):
+    await websocket.accept()
+
+    audio_buffer = bytearray()
+    chunk_duration = 3
+
+    SAMPLE_RATE = 16000  # 16kHz
+    BYTES_PER_SAMPLE = 2  # Int16
+    BYTES_PER_SECOND = SAMPLE_RATE * BYTES_PER_SAMPLE  # 32,000
+
+    async def transcribe_chunk(chunk_data:bytes):
+        "Process transcription in the background "
+        try:
+            # Convert raw PCM to WAV format using built-in wave module
+            audio_file = io.BytesIO()
+
+            # Create WAV file
+            with wave.open(audio_file, 'wb') as wav_file:
+                wav_file.setnchannels(1)  # Mono
+                wav_file.setsampwidth(2)  # 2 bytes = 16-bit
+                wav_file.setframerate(16000)  # 16kHz
+                wav_file.writeframes(chunk_data)
+
+            # Reset to beginning
+            audio_file.seek(0)
+            audio_file.name = "audio.wav"
+
+            #transcribe
+            transcript = client.audio.transcriptions.create(
+                file = audio_file,
+                model = "whisper-1",
+                language = "en"
+            )
+
+            #Send result back to browser
+            await websocket.send_json({
+                "type": "transcription",
+                "text": transcript.text
+            })
+
+        except Exception as e:
+            await websocket.send_json({
+            "type": "error",
+            "message": str(e)
+            })
+
+
+
+    try:
+        while True:
+            # Receive audio chunk from browser
+            data  = await websocket.receive_bytes()
+            audio_buffer.extend(data)
+
+            #When buffer reach 10 sec Transcribe it
+            if len(audio_buffer) > BYTES_PER_SECOND * chunk_duration:
+
+                chunk_to_process = bytes(audio_buffer)
+
+                # Clear buffer IMMEDIATELY for next chunk
+                audio_buffer.clear()
+
+                asyncio.create_task(transcribe_chunk(chunk_to_process))
+                #keep receiving audio from browser with asyncio
+
+    except WebSocketDisconnect:
+        print("Client Disconnected from Websocket")
+    except Exception as e:
+        print(f"Websocket error : {e}")
+        try:
+            await websocket.close()
+        except:
+            pass
+
+
+
+
+
+
+
+
 
 
 
