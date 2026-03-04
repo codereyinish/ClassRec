4b546e949cdbe85b41f0a09d9c77c62a7f6adffb
Inish Codes <inishbista20@gmail.com>
2026-03-04 14:11:43 -0500

    Chore(logger.py): Create logger.py to make the logging more easy and efficient without need to remove individual Print log message

---
+++ src/logger.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def setup_logger()  -> logging.Logger:
+    """Setup ClassRec logger - controlled by DEBUG env variable"""
+
+    log_level = logging.DEBUG if os.getenv("DEBUG") == "true" else logging.WARNING
+
+    logging.basicConfig(
+        level=log_level,
+        format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+        datefmt= '%H:%M:%S'
+    )
+    # silence noisy third party libraries
+    logging.getLogger("httpcore").setLevel(logging.WARNING)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("openai").setLevel(logging.WARNING)
+
+    return logging.getLogger('ClassRec')
+
+logger = setup_logger()
+
+
+
